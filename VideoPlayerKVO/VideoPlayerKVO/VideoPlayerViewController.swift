4c40233d9364a3c15d35b8e6af4f9cf71b3bb25e
littleweh <adakao@gmail.com>
2018-01-19 13:40:32 +0800

    1. install SwiftLint, 2. delete storyboard(Main), 3. add a VideoPlayerVC, and set the backgroundColor black

---
+++ VideoPlayerKVO/VideoPlayerKVO/VideoPlayerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  VideoPlayerViewController.swift
+//  VideoPlayerKVO
+//
+//  Created by 典萱 高 on 2018/1/19.
+//  Copyright © 2018年 LostRfounds. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoPlayerViewController: UIViewController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
