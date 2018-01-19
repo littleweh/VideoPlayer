bf7d4f0612b177a9db894c866b6a8510c1b0d47b
littleweh <adakao@gmail.com>
2018-01-19 14:12:15 +0800

    1. add a videoPlayerContainerView in VC, 2. use AVPlayer to playback the sample video in videoPlayerContainerView (use layer)

--- VideoPlayerKVO/VideoPlayerKVO/VideoPlayerViewController.swift
+++ VideoPlayerKVO/VideoPlayerKVO/VideoPlayerViewController.swift
@@ -15,10 +15,62 @@ class VideoPlayerViewController: UIViewController {
         return .lightContent
     }
 
+    var videoPlayerContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    // searchbar
+
+    // button and container view
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.addSubview(videoPlayerContainerView)
+
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupVideoPlayerContainerView()
+
+    }
+
+    func playVideoWith(url: String) {
+        guard
+            let videoURL = URL(string: url)
+        else {
+            // ToDo: send alert to user and re enter url
+            return
+        }
+
+        let player = AVPlayer(url: videoURL)
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = videoPlayerContainerView.bounds
+        layer.videoGravity = AVLayerVideoGravity.resizeAspect
+        videoPlayerContainerView.layer.addSublayer(layer)
+        player.play()
+    }
+
+    func setupVideoPlayerContainerView() {
+        // autolayout
+        self.videoPlayerContainerView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: self.view.frame.width,
+            height: self.view.frame.height
+        )
+
+        NSLayoutConstraint.activate([
+            videoPlayerContainerView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor)
+        ])
+
+    }
+
+    deinit {
+        print("VideoPlayerViewController deinit")
     }
 
 }
